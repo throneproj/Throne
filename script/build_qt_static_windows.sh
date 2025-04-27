cfb61931034ec47c84580d238fa920ae5969ca06
parhelia512 <0011d3@gmail.com>
2025-04-27 12:39:14 +0800

    Cleanup build scripts

---
+++ script/build_qt_static_windows.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git clone https://code.qt.io/qt/qt5.git qt6
+cd qt6
+
+git switch $1
+mkdir build
+./configure.bat -release -static -prefix ./build -static-runtime -submodules qtbase,qtimageformats,qtsvg,qttranslations -skip tests -skip examples -init-submodules
+echo "on branch $1"
+echo "config complete, building..."
+cmake --build . --parallel
+echo "build one, installing..."
+ninja install
+echo "installed Qt $1 in static mode"
+
+cd ..
